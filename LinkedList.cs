d3c3d104f2a9f3fbdbba67b7b9b6dceafa6dc3ea
ahmet bilgic <ahmet_bilgic@yahoo.com>
2023-07-10 00:47:53 +0300

    Add project files.

---
+++ LinkedList.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReversedLinkedList
+{
+    internal class LinkedList
+    {
+        public Node Root;
+        public class Node
+        {
+            public int Data;
+            public Node Next;
+            public Node(int Data)
+            {
+                this.Data = Data;
+            }
+        }
+        public Node Insert(int Data, Node Head)
+        {
+            if (Head == null)
+            {
+                Head = new Node(Data);
+                return Head;
+            }
+            else
+            {
+                Head.Next = Insert(Data, Head.Next);
+                return Head;
+            }
+
+        }
+        public void Print(Node Head)
+        {
+            if (Head == null)
+                return;
+            Console.Write($"{Head.Data} ");
+            Print(Head.Next);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿var llist = new ReversedLinkedList.LinkedList();
+
+llist.Root = llist.Insert(1, llist.Root);
+llist.Root = llist.Insert(2, llist.Root);
+llist.Root = llist.Insert(3, llist.Root);
+llist.Root = llist.Insert(4, llist.Root);
+llist.Root = llist.Insert(5, llist.Root);
+
+llist.Print(llist.Root);
